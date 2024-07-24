6e612b8a61cf4ff27707809e477bfa2778b8f20a
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-07-24 12:00:38 -0400

    saving to local storage

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import TaskForm from './TaskForm';
 import Task from './Task'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  
+
+  // Saving all tasks to local storage
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks])
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks"));
+    setTasks(tasks || []);
+  }, [])
+
+
+  //Creating tasks 
   function addTask(name) {
     setTasks(prev => {
       return [...prev, {name: name, done:false}]; 
     })
   }
+
   return (
    <main>
     <TaskForm onAdd ={addTask}/>
-    {tasks.map(tasks => (
-      <Task {...tasks}/>
+    {tasks.map(task => (
+      <Task {...task}/>
     ))}
 
    </main>
