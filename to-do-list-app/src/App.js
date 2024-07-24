e5269811fd24d7cd99b75838d26ad96a169921c0
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-07-24 11:40:28 -0400

    Functioning Task list and Checkbox

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import TaskForm from './TaskForm';
 import Task from './Task'
+import {useState} from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name: name, done:false}]; 
+    })
+  }
   return (
    <main>
-    <TaskForm/>
-    <Task/> 
-    <Task/> 
-    <Task/> 
-    <Task/> 
+    <TaskForm onAdd ={addTask}/>
+    {tasks.map(tasks => (
+      <Task {...tasks}/>
+    ))}
+
    </main>
   );
 }
--- to-do-list-app/src/TaskForm.js
+++ to-do-list-app/src/TaskForm.js
@@ -1,10 +1,15 @@
 import React, {useState} from 'react';
 
-export default function TaskForm() {
+export default function TaskForm({onAdd}) {
     const [taskName,setTaskName] = useState('');
+    function handleSubmit(ev) {
+        ev.preventDefault();
+        onAdd(taskName);
+        setTaskName("");
+    }
     return (
-        <form>
-            <button>+</button>
+        <form onSubmit={handleSubmit}>
+            <button>+</button> 
             <input type="text" 
             value = {taskName} 
             onChange={ev => setTaskName(ev.target.value)}
